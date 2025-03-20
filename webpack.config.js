7370e2f5ea1a08d5c7bc9a47a51bebf2af8affd6
lukmanAkhmad <145940997+lukmanAkhmad@users.noreply.github.com.>
2025-03-20 17:10:43 +0700

    loading images

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,15 @@ module.exports = {
         rules: [
             {
                 test: /\.css$/i,
-                use: ["style-loader","css-loader"],
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
             },
         ],
     },
