4d0bc6f65a9d7d56ede282380a75cbc0bfefaf5f
lukmanAkhmad <145940997+lukmanAkhmad@users.noreply.github.com.>
2025-05-19 06:57:24 +0700

    install eslint-config-prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
+import someConfig from "some-other-config-you-use";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 export default defineConfig([
   {
@@ -18,4 +20,6 @@ export default defineConfig([
       "no-undef": "warn",
     },
   },
+  someConfig,
+  eslintConfigPrettier,
 ]);
